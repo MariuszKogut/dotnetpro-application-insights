a6a9da15b9fb24d3e23a11fd9df7e22594c13b3c
Mariusz Kogut <mk@handmade-systems.de>
2020-01-19 18:03:24 +0100

    Add delete confirmation

--- frontend/src/shared/components/customer-card.tsx
+++ frontend/src/shared/components/customer-card.tsx
@@ -13,6 +13,12 @@ const CustomerCard: FunctionComponent<Props> = props => {
     onDelete
   } = props;
 
+  const handleDeleteClick = () => {
+    if (id && window.confirm(`Möchten Sie '${name}' wirklich löschen?`)) {
+      onDelete(id);
+    }
+  };
+
   return (
     <div className="card">
       <div className="card-header font-weight-bold">{name}</div>
@@ -28,14 +34,9 @@ const CustomerCard: FunctionComponent<Props> = props => {
         <Link className="btn btn-secondary" to={`/customer/editor/${id}`}>
           Öffnen
         </Link>
-        {id && (
-          <button
-            className="btn btn-danger ml-auto"
-            onClick={() => onDelete(id)}
-          >
-            Löschen
-          </button>
-        )}
+        <button className="btn btn-danger ml-auto" onClick={handleDeleteClick}>
+          Löschen
+        </button>
       </div>
     </div>
   );
